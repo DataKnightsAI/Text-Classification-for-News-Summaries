4c4d253a4e0fa0c745c64947c690f668c0dc84a8
Tony Lee <22460852+ssbmaster@users.noreply.github.com>
2020-04-18 11:12:00 -0400

    f1 curve plots generated.
    Co-authored-by: viswesh-ml <viswesh@my.yorku.ca>

--- feature-engineering.py
+++ feature-engineering.py
@@ -30,6 +30,7 @@ from sklearn import svm # Support Vector Machine
 from sklearn.preprocessing import label_binarize # Split category encoding eg. y=[1,2,3] into y1=[0,1], y2=[0,1], y3=[0,1]
 from sklearn.model_selection import train_test_split # Built-in train test splitter
 from sklearn.multiclass import OneVsRestClassifier # We use OneVsRestClassifier for multi-label prediction
+from itertools import cycle
 
 # %% [markdown]
 # ## Load in the data from the database
@@ -375,7 +376,7 @@ tokenized_corpus_train = [wpt.tokenize(document) for document in x_train]
 tokenized_corpus_test = [wpt.tokenize(document) for document in x_test]
 
 # Set values for various parameters
-feature_size = 5000    # Word vector dimensionality  
+feature_size = 4000    # Word vector dimensionality  
 window_context = 20          # Context window size      
 workers = 12                                                                              
 min_word_count = 5   # Minimum word count                        
@@ -498,7 +499,6 @@ def run_svm(x_train, y_train, x_test, emb):
     print('Average precision score, micro-averaged over all classes: {0:0.2f}'
         .format(average_precision["micro"]))
 
-
     # Plot the micro-averaged Precision-Recall curve
     plt.figure()
     plt.step(recall['micro'], precision['micro'], where='post')
@@ -510,6 +510,44 @@ def run_svm(x_train, y_train, x_test, emb):
     plt.title(
         'Average precision score for, micro-averaged over all classes: AP={0:0.2f}'
         .format(average_precision["micro"]))
+    
+    # Plot Precision-Recall curve for each class and iso-f1 curves
+    # setup plot details
+    colors = cycle(['navy', 'turquoise', 'darkorange', 'cornflowerblue', 'teal'])
+
+    plt.figure(figsize=(7, 8))
+    f_scores = numpy.linspace(0.2, 0.8, num=4)
+    lines = []
+    labels = []
+    for f_score in f_scores:
+        x = numpy.linspace(0.01, 1)
+        y = f_score * x / (2 * x - f_score)
+        l, = plt.plot(x[y >= 0], y[y >= 0], color='gray', alpha=0.2)
+        plt.annotate('f1={0:0.1f}'.format(f_score), xy=(0.9, y[45] + 0.02))
+
+    lines.append(l)
+    labels.append('iso-f1 curves')
+    l, = plt.plot(recall["micro"], precision["micro"], color='gold', lw=2)
+    lines.append(l)
+    labels.append('micro-average Precision-recall (area = {0:0.2f})'
+                ''.format(average_precision["micro"]))
+
+    for i, color in zip(range(n_classes), colors):
+        l, = plt.plot(recall[i], precision[i], color=color, lw=2)
+        lines.append(l)
+        labels.append('Precision-recall for class {0} (area = {1:0.2f})'
+                    ''.format(i, average_precision[i]))
+
+    fig = plt.gcf()
+    fig.subplots_adjust(bottom=0.25)
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.title('Extension of Precision-Recall curve to multi-class')
+    plt.legend(lines, labels, loc=(0, -.5), prop=dict(size=14))
+
+    plt.show()
 
 #%%
 run_svm(x_train_bagofwords, y_train, x_test_bagofwords, 'Bag of Words')
@@ -525,9 +563,6 @@ run_svm(x_train_tfidf_unigram, y_train, x_test_tfidf_unigram, 'TF/IDF Unigram')
 
 #%%
 run_svm(x_train_tfidf_ngram, y_train, x_test_tfidf_ngram, 'TF/IDF N-Grams')
-
-#%%
-run_svm(x_train_tfidf_char, y_train, x_test_cv_char, 'TF/IDF Chars')
 
 #%%
 run_svm(x_train_tfidf_char, y_train, x_test_cv_char, 'TF/IDF Chars')
